f03b0ae72f71c8f024c69f04cc8977aa1c9c288f
kumdan job <redeks123456@gmail.com>
2023-02-16 09:14:57 -0800

    put 2

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 /*
  * the main function prints out the string in the pust function
  */
-int main(void) 
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
