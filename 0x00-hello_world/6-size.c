7991272c5856b70881a0c496514f88fd136c2476
kumdan job <redeks123456@gmail.com>
2023-02-16 12:01:28 -0800

    number 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
-/*
- * the main function prints out the string in the pust function
- */
+/**
+ * main - program that prints the size of various types of bytes
+ *
+ * Return: Always return 0.
+*/
 int main(void)
 {
-    int a;
-    float b;
-    char c;
-    long int d;
-    long long int e;
-
-    printf("Size of a long int: %d byte(s)\n", sizeof(int));
-    printf("Size of a char: %d byte(s)\n", sizeof(char));
-    printf("Size of a float: %d byte(s)\n", sizeof(float));
-    printf("Size of a long int: %d byte(s)\n", sizeof(long int));
-    printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
-    
-	return (0);
+    	int a;
+	float b;
+    	char c;
+    	long int d;
+    	long long int e;
+    	printf("Size of a long int: %d byte(s)\n", sizeof(int));
+    	printf("Size of a char: %d byte(s)\n", sizeof(char));
+	printf("Size of a float: %d byte(s)\n", sizeof(float));
+	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+       	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+    	return (0);
 }
