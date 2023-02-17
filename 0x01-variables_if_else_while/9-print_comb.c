de57cc29e7322156e8e121a9600cf4534d94866f
job kumdan <redeks123456@gmail.com>
2023-02-17 16:24:10 +0100

    here we go

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,9 +7,11 @@
 int main(void) {
 	int i;
 	    for(i = 48; i <= 57; i++){
-        putchar(i);
+             putchar(i);
+        if(i < 57){
         putchar(',');
         putchar(' ');
+        }
     }
 	putchar('\n');
 	return 0;
