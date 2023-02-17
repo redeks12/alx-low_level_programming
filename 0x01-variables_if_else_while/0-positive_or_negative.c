f6ddf1b3fb71e3a6c2d7d0a31cd704fd98b1e87f
kumdan job <redeks123456@gmail.com>
2023-02-17 14:05:47 -0800

    pk

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
-/* betty style doc for function main goes there */
+/**
+ * main - program that prints all leters in the alphabet
+ * Return: Always return 0.
+*/
 int main(void)
 {
 	int n;
--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,24 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
+/**
+ * main - program that prints all leters in the alphabet
+ * Return: Always return 0.
+*/
 int main(void)
 {
 	int n, last_digit;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
 	last_digit = n % 10;
 	if (last_digit > 5)
 	{
 		printf("Last digit of %d is %d and is greater than 5\n", n, last_digit);
-	} else if (last_digit == 0)
+	}
+	else if (last_digit == 0)
         {
                 printf("Last digit of %d is %d and is 0\n", n, last_digit);
-        } else if (last_digit < 6 && last_digit != 0)
+        }
+	else if (last_digit < 6 && last_digit != 0)
         {
                 printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_digit);
         }
