1c2f9ad49704cc6daa335aa745d3dac2c3a5fdca
job kumdan <redeks123456@gmail.com>
2023-02-21 21:08:29 +0100

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - program that prints all leters in the alphabet
+ * Return: Always return 0.
+*/
+void print_alphabet(void);
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
+void print_alphabet(void)
+{
+	printf("_putchar\n");
+}
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - program that prints all leters in the alphabet
+ * Return: Always return 0.
+*/
+void print_alphabet_x10(void);
+int main(void)
+{
+	print_alphabet_x10();
+	return (0);
+}
+void print_alphabet_x10(void)
+{
+	int i;
+
+	while(i = 0)
+	{
+		printf("abcdefghijklmnopqrstuvwxyz\n");
+		i++;
+	}
+}
