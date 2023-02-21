98b61fb07f34bc0eb2ff5c3d19a2359f4e7d25ba
job kumdan <redeks123456@gmail.com>
2023-02-21 20:54:55 +0100

    task 1

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+/**
+ * main - program that prints all leters in the alphabet
+ * Return: Always return 0.
+*/
 int main(void)
 {
 	printf("_putchar\n");
-	return(0);
+	return (0);
 }
 
