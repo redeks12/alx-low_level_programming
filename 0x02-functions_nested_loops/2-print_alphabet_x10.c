fd4b157123e4b2612af0ffe82fbd6cbebeae7455
job kumdan <redeks123456@gmail.com>
2023-02-22 00:30:59 +0100

    testing header file

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
- * print_alphabet_x10 - this prints out all the letters of the alphabet in order ten times
+ * print_alphabet_x10 - this prints out all the letters of the alphabet
+ * in order ten times
  * using a nested while loop and the putchar function
 */
 void print_alphabet_x10(void)
