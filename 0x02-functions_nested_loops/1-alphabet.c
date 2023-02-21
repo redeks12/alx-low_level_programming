e51fdc59031ac76f332590f3eb17971b12bff934
job kumdan <redeks123456@gmail.com>
2023-02-22 00:20:12 +0100

    testing header file

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * this prints out all the letters of the alphabet in order
+ * print_alphabet - this prints out all the letters of the alphabet in order
  * using a while loop and the putchar function
 */
 void print_alphabet(void)
