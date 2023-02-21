3f1cbda71e542bc59679d43860f8d27e7c80f0c4
job kumdan <redeks123456@gmail.com>
2023-02-21 21:40:42 +0100

    task 1

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,18 +5,27 @@
  * Return: Always return 0.
 */
 void print_alphabet_x10(void);
-int main(void)
-{
-	print_alphabet_x10();
-	return (0);
+int main(void){
+
+        print_alphabet_x10();
+        return (0);
 }
+
+
 void print_alphabet_x10(void)
 {
-	int i = 0;
+        int i = 0;
 
-	while(i < 10)
-	{
-		printf("abcdefghijklmnopqrstuvwxyz\n");
-		i++;
-	}
+        while(i <= 10)
+        {
+            char a = 'a';
+             while(a <= 'z');
+             {
+                 putchar(a);
+                 a++;
+             }
+             i++;
+             putchar('\n');
+        }
 }
+
