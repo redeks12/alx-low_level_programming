c44c128d89e5dfceeea424ba40e99251919219ec
job kumdan <redeks123456@gmail.com>
2023-02-21 23:59:11 +0100

    testing header file

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,11 +3,6 @@
  * main - program that prints all leters in the alphabet
  * Return: Always return 0.
 */
-int main(void)
-{
-	print_alphabet();
-	return (0);
-}
 void print_alphabet(void)
 {
 	_putchar('\n');
