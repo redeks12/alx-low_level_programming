2294747981a226f66f23606366705ac6b68d417c
job kumdan <redeks123456@gmail.com>
2023-02-22 04:41:43 +0100

    last

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -7,10 +7,7 @@
 int _isalpha(int c)
 {
 	if (c >= 65 && c <= 122)
-	{
 		return (1);
-	} else
-	{
+	else
 		return (0);
-	}
 }
--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -7,17 +7,12 @@
 int print_sign(int n)
 {
 	if (n > 0)
-	{
 		_putchar('+');
 		return (1);
-	} else if (n < 0)
-	{
+	else if (n < 0)
 		_putchar('-');
 		return (-1);
-	} else
-	{
+	else
 		_putchar('0');
 		return (0);
-	}
-
 }
