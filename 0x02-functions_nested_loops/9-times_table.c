406de6beade7d35c64ca96b55dfbb3dad804106c
job kumdan <redeks123456@gmail.com>
2023-02-22 02:27:44 +0100

    omo

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,9 +9,9 @@ void times_table(void)
 	int i;
 	int j;
 
-	for (i = 0;i <= 9; i++)
+	for (i = '0';i <= '9'; i++)
 	{
-		for (j = 0;j <= 9; j++)
+		for (j = '0';j <= '9'; j++)
 		{
 			_putchar(i * j);
 			_putchar(',');
