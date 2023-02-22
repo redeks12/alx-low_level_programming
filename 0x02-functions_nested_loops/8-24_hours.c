3574dd9ad5746bdd611f9d94f4cecb833b5741f3
job kumdan <redeks123456@gmail.com>
2023-02-22 03:40:38 +0100

    omo

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,22 +6,26 @@
 */
 void jack_bauer(void)
 {
-	int i;
-	int j;
+	        int i;
+        int j;
 
-	for (i = 0; i <= 24; i++)
-	{
-		for (j = 0; j <= 60; j++)
-		{
-			if (i < 10)
-			{
-				_putchar(0);
-			}
-			_putchar(i);
-			_putchar(':');
-			if (j < 10)
-				_putchar(0);
-			_putchar(j);
-		}
-	}
+        for (i = 0; i < 24; i++)
+        {
+                for (j = 0; j < 60; j++)
+                {
+
+                        if(i < 10)
+                        {
+                        _putchar(0);
+                        }
+                        _putchar(i);
+                        _putchar(':');
+                        if(j < 10)
+                        {
+                        _putchar(0);
+                        }
+                        _putchar(j);
+			_putchar('\n');
+                }
+        }
 }
