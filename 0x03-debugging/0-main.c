8c003a84ca9a625e3167897e423f70dadebffc41
job kumdan <redeks123456@gmail.com>
2023-02-23 15:01:27 +0100

    touches

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,2 +1,10 @@
 #include "main.h"
-positive_or_negative(0);
+/*
+ * main - tests the positive negative function with value 0*/
+int main(void)
+{
+	int n = 0;
+
+	positive_or_negative(n);
+	return (0);
+}
