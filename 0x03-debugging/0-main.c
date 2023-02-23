d686c26a3a41940e918bc2238defedac167e5756
job kumdan <redeks123456@gmail.com>
2023-02-23 18:42:03 +0100

    here we go

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include "main.h"
 /*
- * main - tests the positive negative function with value 0 */
-
+ * main - tests the positive negative function with value 0 
+ * return 0
+ */
 int main(void)
 {
 	int n = 0;
 
 	positive_or_negative(n);
+
 	return (0);
 }
