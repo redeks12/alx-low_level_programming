03bf58784186fd83fd7bb4fb3bf763dba319a0c2
job kumdan <redeks123456@gmail.com>
2023-03-01 16:08:44 +0100

    firts

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/*
+ * _strcat - this function takes in 2 arguments and joins them together
+ * @dest - fist argument
+ * @src - second argument
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i; 
+
+	int j = strlen(dest);
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[i + j] = src[i];
+	}
+	dest[i + j] = '\0';
+
+	return (dest);
+}
