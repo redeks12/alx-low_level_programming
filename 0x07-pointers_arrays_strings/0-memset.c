36ba701dca4217452b1381dc4787642d7809bf5c
job kumdan <redeks123456@gmail.com>
2023-03-07 00:21:35 +0100

    we back

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -4,7 +4,8 @@
  * @s : first str
  * @b : second str
  * @n : integer
- * Return : Always s
+ *
+ * Return: If s can store the sum - a pointer to the result.
  */
 char *_memset(char *s, char b, unsigned int n)
 {
