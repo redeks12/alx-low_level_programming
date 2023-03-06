c449a784a85ad2ca3c724a0a22fb7217f36e80b9
job kumdan <redeks123456@gmail.com>
2023-03-07 00:08:15 +0100

    we back

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/*
+ * _memset - function takes fills n amt
+ * @s - first str
+ * @b - second str
+ * @n - integer
+ * return - s*/
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+
+	return (s);
+}
