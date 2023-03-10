6b816dbc44265f9444f1b8f3ab0a7ee6ec46beb5
job kumdan <redeks123456@gmail.com>
2023-03-10 13:22:35 +0100

    ....

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/*
+ * main - prints arguments
+ * @argc : first argument
+ * @*argv[] : second argument
+ * return : 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s", argv[0]);
+	return (0);
+}
