0dd0badb2766b8a0265beda73ee861075d693696
job kumdan <redeks123456@gmail.com>
2023-03-10 14:07:51 +0100

    ....

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -2,9 +2,10 @@
 /**
  * main - Prints the program name, followed by a new line.
  * @argc : first argument
+ * *argv[] : second argument
  * return : 0
  */
-int main(int argc, char __attribute__((unused)))
+int main(int argc, char __attribute__((unused)) *argv[])
 {
 	printf("%d\n", argc);
 	return (0);
