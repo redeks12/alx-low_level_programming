4aa401b984b6c8591e2f54bff2f5c4dc4d98cf09
job kumdan <redeks123456@gmail.com>
2023-03-10 14:28:33 +0100

    ....

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Prints the program name, followed by a new line.
+ * @argc : first argument
+ * @*argv[] : second argument
+ * return : 0
+ */
+int main(int argc, char *argv[])
+{
+	if (argc == 2)
+	{
+		printf("%d\n", argv[0] * argv[1]);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+	return (0);
+}
