af2d1f6ce722ac46b56526dd1a1ab6041cd12f88
job kumdan <redeks123456@gmail.com>
2023-03-20 15:07:05 +0100

    new project on struct

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "dog.h"
+
+void print_dog(struct dog *d)
+{
+        if (d == NULL)
+                return;
+
+        if (d->name != NULL)
+                printf("Name: %s\n", d->name);
+        else
+                printf("Name: (nil)\n");
+        if (d->age)
+                printf("Age: %s\n", d->age);
+        else
+                printf("Age: (nil)\n");
+        if (d->owner != NULL)
+                printf("Owner: %s\n", d->owner);
+        else
+                printf("Owner: (nil)\n");
+ 
+}
