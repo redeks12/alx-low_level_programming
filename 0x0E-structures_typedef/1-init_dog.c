49925d35f5cccddf97adc49c78da8ff269c23a47
job kumdan <redeks123456@gmail.com>
2023-03-20 15:18:13 +0100

    new project on struct

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,7 +1,10 @@
 #include "dog.h"
+#include <stdlib.h>
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+        if (d == NULL)
+                return;
         d->name = name;
         d->age = age;
         d->owner = owner;
