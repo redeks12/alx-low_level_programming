1255d56b3b158a5c678c4b6a1b7cfd85c3b29f03
job kumdan <redeks123456@gmail.com>
2023-03-23 14:07:36 +0100

    new project on VARIADIC FUNCTIONS

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,21 @@
+#include "variadic_functions.h"
+#include <stdlib.h>
+#include <stdarg.h>
+
+int sum_them_all(const unsigned int n, ...)
+{
+        if (n == 0)
+                return (0);
+        va_list args;
+        va_start(args, n);
+
+        int i;
+        int s = 0;
+        for (i = 0; i < n; i++)
+        {
+                int x = va_arg(args, int);
+                s += x;
+        }
+        va_end(args);
+        return (s);
+}
