4e33ff3c19d817eb1781b0afd2cce095f6cb71e6
job kumdan <redeks123456@gmail.com>
2023-03-23 15:39:15 +0100

    new project on VARIADIC FUNCTIONS

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,24 +1,29 @@
 #include "variadic_functions.h"
-#include <stdarg.h>
 #include <stdio.h>
+#include <stdarg.h>
 
+/**
+ * print_numbers - Prints numbers, followed by a new line.
+ * @separator: The string to be printed between numbers.
+ * @n: The number of integers passed to the function.
+ * @...: A variable number of numbers to be printed.
+ */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-        va_list args;
-        unsigned int i;
-        
-        if (separator == NULL)
-                return;
+	va_list nums;
+	unsigned int index;
 
-        va_start(args, n);
+	va_start(nums, n);
 
-        for (i = 0; i < n ; i++)
-        {
-                printf("%d", va_arg(args, int));
-                if (i != n - 1 && (separator != NULL))
-                        printf("%c", *(separator));
+	for (index = 0; index < n; index++)
+	{
+		printf("%d", va_arg(nums, int));
 
-        }
-        printf("\n");
-        va_end(args);
+		if (index != (n - 1) && separator != NULL)
+			printf("%s", separator);
+	}
+
+	printf("\n");
+
+	va_end(nums);
 }
--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,30 +1,38 @@
 #include "variadic_functions.h"
-#include <stdarg.h>
 #include <stdio.h>
-#include <stdlib.h>
+#include <stdarg.h>
 
+/**
+ * print_strings - Prints strings, followed by a new line.
+ * @separator: The string to be printed between strings.
+ * @n: The number of strings passed to the function.
+ * @...: A variable number of strings to be printed.
+ *
+ * Description: If separator is NULL, it is not printed.
+ *              If one of the strings if NULL, (nil) is printed instead.
+ */
 void print_strings(const char *separator, const unsigned int n, ...)
 {
-        va_list args;
-        unsigned int i;
-        char *x;
-        
-        if (separator == NULL)
-                return;
+	va_list strings;
+	char *str;
+	unsigned int index;
 
-        va_start(args, n);
+	va_start(strings, n);
 
-        for (i = 0; i < n ; i++)
-        {
-                x = va_arg(args, char*);
-                if (x != NULL)
-                        printf("%s", x);
-                else
-                        printf("(nil)");
-                if ((i != n - 1) && (separator != NULL))
-                        printf("%c", *(separator));
+	for (index = 0; index < n; index++)
+	{
+		str = va_arg(strings, char *);
 
-        }
-        printf("\n");
-        va_end(args);
+		if (str == NULL)
+			printf("(nil)");
+		else
+			printf("%s", str);
+
+		if (index != (n - 1) && separator != NULL)
+			printf("%s", separator);
+	}
+
+	printf("\n");
+
+	va_end(strings);
 }
