877442fefb4374061b4e1f8224c6a1af942cc5d1
job kumdan <redeks123456@gmail.com>
2023-03-23 15:32:45 +0100

    new project on VARIADIC FUNCTIONS

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -22,7 +22,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
                 else
                         printf("(nil)");
                 if ((i != n - 1) && (separator != NULL))
-                        printf("%c ", *(separator));
+                        printf("%c", *(separator));
 
         }
         printf("\n");
