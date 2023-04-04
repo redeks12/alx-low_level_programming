c1bef345bd0672e182a741537f77ec158edba282
job kumdan <redeks123456@gmail.com>
2023-04-04 23:48:52 +0100

    NEW PROJECT ON MORE SINGLY LINKED LISTS

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -16,7 +16,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
         while (temp->next != NULL)
         {
-                if (i == idx)
+                if (i == idx - 1)
                 {
                         break;
                 }
