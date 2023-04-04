4d246a3e9487a977db6f8e685e9def0cafbb2675
job kumdan <redeks123456@gmail.com>
2023-04-04 14:49:33 +0100

    NEW PROJECT ON MORE SINGLY LINKED LISTS

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -11,7 +11,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
         if (*head == NULL)
         {
                 *head = last;
-                return (last);
+                return (*head);
         }
 
         while (temp->next != NULL)
