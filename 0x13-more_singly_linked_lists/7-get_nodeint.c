24d86cc3b10403cfa9d208c3e2cae701f494d697
job kumdan <redeks123456@gmail.com>
2023-04-04 17:27:08 +0100

    NEW PROJECT ON MORE SINGLY LINKED LISTS

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+        unsigned int i = 0;
+        listint_t *temp;
+        temp = head;   
+
+        if (head == NULL)
+        {
+                return NULL;
+        }
+
+
+        while (temp->next != NULL)
+        {
+                if (i == index)
+                {
+                        break;
+                }
+                i++;
+                temp =  temp->next;
+
+                
+        }
+        return (temp);
+}
