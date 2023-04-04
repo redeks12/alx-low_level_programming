1c45d9814aff9193980d92d0001718b279fce861
job kumdan <redeks123456@gmail.com>
2023-04-04 16:06:06 +0100

    NEW PROJECT ON MORE SINGLY LINKED LISTS

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,7 +9,7 @@ void free_listint2(listint_t **head)
 
         if (temp == NULL)
         {
-                *head = NULL;
+                temp = NULL;
                 return;
         }
         
