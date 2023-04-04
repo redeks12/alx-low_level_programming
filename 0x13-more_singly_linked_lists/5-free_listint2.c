6dd3fbacfafa7a545026440f1870911e03cbcd32
job kumdan <redeks123456@gmail.com>
2023-04-04 16:01:00 +0100

    NEW PROJECT ON MORE SINGLY LINKED LISTS

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+
+void free_listint2(listint_t **head)
+{
+        listint_t *temp;
+        listint_t *temp2;
+        temp = *head;
+
+        if (temp == NULL)
+        {
+                return;
+        }
+        
+        while (temp != NULL)
+        {
+                temp2 = temp;
+                temp = temp->next;
+                free(temp2);
+        }
+        free(temp);
+        *head = NULL;
+}
