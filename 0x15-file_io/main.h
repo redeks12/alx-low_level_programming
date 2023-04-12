d38d310da453f93e53837a95de839913f7875340
job kumdan <redeks123456@gmail.com>
2023-04-12 18:31:34 +0100

    beginig to understand

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -2,7 +2,7 @@
 #define MAIN_H
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <unistd.h>
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int _putchar(char c);
