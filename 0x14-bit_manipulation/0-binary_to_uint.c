f894357727cef07d3cb522a4badcf1e1500f0f10
job kumdan <redeks123456@gmail.com>
2023-04-13 23:19:24 +0100

    beginig to understand

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,28 +1,34 @@
+#include <stdio.h>
+#include <string.h>
 #include "main.h"
+#include <math.h>
+
+/**
+ * binary_to_uint - Its converts a bianry number to an unsigned int
+ * @b: pointer to the array of characters in the binary to be converted
+ *
+ * Return: the converted  number, or 0 if
+ * there is more then one chars in the string
+ * that is not 0 or 1
+ */
 
 unsigned int binary_to_uint(const char *b)
 {
-        unsigned int num = atoi(b);
-        unsigned int dec_value = 0;
-        
-        unsigned int base = 1;
-        unsigned int temp = num;
+	int i, base = 1, len = 0;
+	unsigned int res = 0;
 
-        if (num == 0)
-        {
-                return (0);
-        }
-        while (*b) if (isdigit(*b++) == 0) return (0);
+	if (!b)
+		return (0);
 
-        while (temp) 
-        {
-                int last_digit = temp % 10;
-                temp = temp / 10;
-        
-                dec_value += last_digit * base;
-  
-                base = base * 2;
-        }
-        return dec_value;
+	len = strlen(b);
 
+	for (i = len - 1; i >= 0; i--)
+	{
+		if (b[i] < '0' || b[i] > '1')
+			return (0);
+		if (b[i] == '1')
+			res += base;
+		base *= 2;
+	}
+	return (res);
 }
--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,14 +1,14 @@
-#include <main.h>
+#include "main.h"
 
+/**
+ * print_binary - It prints the binary representation of a number
+ * @n: the number to be used
+ *
+ * Return: void
+ */
 void print_binary(unsigned long int n)
 {
-        unsigned long int binNum = 0;
-        int power = 0;
-        int rem;
-
-        while (n > 0)
-        {
-                rem = n % 2;
-        }
-        
+	if (n > 1)
+		print_binary(n >> 1);
+	_putchar((n & 1) + '0');
 }
