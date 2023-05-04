a11cb0d551bddc9fa8b19d08d6501b4c8cdac12c
kumdan job <redeks123456@gmail.com>
2023-05-04 23:34:00 +0100

    first

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,33 +1,27 @@
 #include "main.h"
 /**
- * binary_to_uint - convert binary to unsigned int
- * @b: binary
- * Return: unsigned int
+ * binary_to_uint - translate binary to unsigned int
+ * @b: string to convert
+ *
+ * Return: Number, or 0 if b is nan
  */
 unsigned int binary_to_uint(const char *b)
 {
-
-	int length = 0, i;
-	unsigned int sum = 0;
+	unsigned int number;
+	int i, flag;
 
 	if (b == NULL)
-		return (sum);
-
-	while (b[length] != '\0')
-		length++;
-	length -= 1;
-
-	i = 0;
-	while (b[i])
+		return (0);
+	for (number = 0, flag = 0, i = 0; b[i] != '\0'; i++)
 	{
-		if ((b[i] != '0') && (b[i] != '1'))
-			return (sum);
-
-		if (b[i] == '1')
-			sum += (1 * (1 << length));
-		i++;
-		length--;
+		if (b[i] != '1' && b[i] != '0')
+			return (0);
+		if (!flag && b[i] == '1')
+			flag = 1;
+		if (flag && b[i] == '1')
+			number = number << 1, number += 1;
+		else if (flag && b[i] == '0')
+			number = number << 1;
 	}
-
-	return (sum);
+	return (number);
 }
