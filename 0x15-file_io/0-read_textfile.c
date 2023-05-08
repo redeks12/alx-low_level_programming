4d6a8e8cae300913a2ff7781dcb550d113dc1bf2
kumdan job <redeks123456@gmail.com>
2023-05-08 21:41:56 +0100

    first

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -21,17 +21,25 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		free(buffer);
 		return (0);
 	}
+
 	file = open(filename, O_RDONLY);
+
 	if (file == -1)
 		return (0);
+
 	n_read = read(file, buffer, letters);
+
 	if (n_read == -1)
 		return (0);
 	buffer[n_read] = '\0';
+
 	wrote = write(STDOUT_FILENO, buffer, n_read);
+
 	if (wrote != n_read)
 		return (0);
+
 	free(buffer);
 	close(file);
+	
 	return (n_read);
 }
