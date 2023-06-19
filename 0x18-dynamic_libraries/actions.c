895545b2d351a6a5517a6d175cb52ff750d17fa8
kumdan job <redeks123456@gmail.com>
2023-06-19 21:41:47 +0100

    next

---
+++ 0x18-dynamic_libraries/actions.c
@@ -0,0 +1,20 @@
+int mul(int a, int b)
+{
+        return a * b;
+}
+int add(int a, int b)
+{
+        return a + b;
+}
+int sub(int a, int b)
+{
+        return a - b;
+}
+int div(int a, int b)
+{
+        return a / b;
+}
+int mod(int a, int b)
+{
+        return a % b;
+}
